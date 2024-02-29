0ff80cad12be48626df260cf73ef77d761bca0dd
JordyAraujo <jordyaraujo@outlook.com>
2024-02-28 23:03:18 -0300

    Feature: Adds ball and pads to the screen

--- main.py
+++ main.py
@@ -12,7 +12,35 @@ small_font = pygame.font.Font('assets/fonts/font.ttf', 24)
 
 pygame.display.set_caption("pygame-pong")
 
+PADS_WIDTH = 20
+PADS_HEIGHT = 90
+BALL_SIZE = 20
+
+ball = pygame.Rect(
+    (WINDOW_WIDTH / 2) - (BALL_SIZE / 2),
+    (WINDOW_HEIGHT / 2) - (BALL_SIZE / 2),
+    BALL_SIZE,
+    BALL_SIZE,
+)
+
+left_pad = pygame.Rect(
+    10,
+    (WINDOW_HEIGHT / 2) - (PADS_HEIGHT / 2),
+    PADS_WIDTH,
+    PADS_HEIGHT
+)
+
+right_pad = pygame.Rect(
+    WINDOW_WIDTH - PADS_WIDTH - 10,
+    (WINDOW_HEIGHT / 2) - (PADS_HEIGHT / 2),
+    PADS_WIDTH,
+    PADS_HEIGHT,
+)
+
+pads = [left_pad, right_pad]
+
 while True:
+
     event = pygame.event.poll()
 
     if (
@@ -28,5 +56,10 @@ while True:
     text = small_font.render("Hello Pong!", True, WHITE)
     text_rect = text.get_rect(center=(WINDOW_WIDTH / 2, 24))
     screen.blit(text, text_rect)
+
+    pygame.draw.rect(screen, WHITE, ball)
+
+    for pad in pads:
+        pygame.draw.rect(screen, WHITE, pad)
 
     pygame.display.flip()
